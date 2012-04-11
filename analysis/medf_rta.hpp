28e45e55ea6069ccfa42e104e7997ca7ae9cdb16
Giuseppe Lipari <g.lipari@sssup.it>
2012-04-11 14:37:16 +0200

    Modified the implementation of the GEDF analysis. Now the two simple tests work perfectly.

--- analysis/medf_rta.hpp
+++ analysis/medf_rta.hpp
@@ -3,9 +3,140 @@
 
 #include <vector>
 #include <models/task.hpp>
+#include <models/has_slack.hpp>
+#include <analysis/fp_response_time.hpp>
 
 namespace Scan {
-    bool rta_compl_edf(int n_proc, std::vector<Task> &task);
+    bool gedf_iterative(int n_proc, std::vector<Task> &task);
+
+    typedef TaskEx<HasSlack> MEDF_Task;
+
+    /** 
+        Helper function for gedf_iterative()
+        
+        It computes the interference of task ti on task tk
+    */
+    template<class TaskType>
+    double gedf_interf_i_k(const TaskType &tk, const TaskType &ti, int n_proc)
+    {
+        int n = int( floor( double(tk.get_dline()) / double(ti.get_period()) ) );
+        double x =  max(0.0, (tk.get_dline() - ti.get_slack() - n * ti.get_period() ) );
+        return n * ti.get_wcet() +  min(ti.get_wcet(), x); 
+    }
+
+    /**
+       Helper function for gedf_iterative()
+
+       It computes a lower bound on the slack of task tk.
+     */
+    template<class TaskType, class Iter> 
+    double gedf_slack_k(const TaskType &tk, Iter tset_begin, Iter tset_end, int n_proc)
+    {
+        double sum = 0.0;
+        for (auto ti = tset_begin; ti != tset_end; ++ ti) 
+            if (ti->get_id() != tk.get_id()) 
+                sum += min(gedf_interf_i_k(tk, *ti, n_proc), tk.get_dline() - tk.get_wcet() + 1);
+        
+        return tk.get_dline() - tk.get_wcet() - floor( sum / double(n_proc) );   
+    }
+
+    /** 
+        RTA Analysis of GEDF.
+      
+        M. Bertogna, M. Cirinei, and G. Lipari, “Schedulability
+        Analysis of Global Scheduling Algorithms on Multiprocessor
+        Platforms,” IEEE Transactions on Parallel and Distributed
+        Systems, vol. 20, no. 4, pp. 553-566, Apr. 2009.
+    */
+    template <class Iter>
+    bool gedf_iterative(int n_proc, Iter v_begin, Iter v_end)
+    {
+        std::vector<MEDF_Task> tset(v_begin, v_end);
+        bool stop = false;
+        bool feasible = false;
+        
+        for (auto k = tset.begin(); k != tset.end(); ++k) 
+            k->set_slack(0);
+
+        while (!stop && !feasible) {
+            stop = true;
+            feasible = true;
+            for (auto k=tset.begin(); k != tset.end(); ++k) {
+                double sb = gedf_slack_k(*k, tset.begin(), tset.end(), n_proc);
+                if (sb < 0) feasible = false;
+                if (sb > k->get_slack()) {
+                    k->set_slack(sb);
+                    stop = false;
+                }
+            }
+        }
+        // exits because stop = true (no task updated) or because
+        // feasible = true (all tasks are schedulable). Notice that if
+        // some sb < 0 and stop = true, the system is deemed not
+        // schedulable
+        return feasible;
+    }
+
+
+    /**
+       Fixed priority schedulability test based on density bound.
+
+       J. Goossens, S. Funk, and S. Baruah, “Priority-driven
+       scheduling of periodic task systems on multiprocessors,” Real
+       Time Systems, vol. 25, no. 2–3, pp. 187–205, 2003.
+    */
+    double lambda(const Task &t)
+    {
+        return (t.get_wcet()/t.get_dline());
+    }
+
+    template <class Iter>
+    bool gedf_bound_gfb(int n_proc, Iter tset_begin, Iter tset_end)
+    {
+	double beta_tot=0, lambda_max=0, lim=0;
+        
+	for(auto k=tset_begin; k != tset_end; k++) {
+            beta_tot += lambda(*k);
+
+            if (lambda(*k) > lambda_max){
+                lambda_max = lambda(*k);
+                lim = n_proc*(1-lambda_max)+lambda_max;
+            }
+            
+            if (beta_tot > lim)
+		return false;
+	}
+	return true;
+    }
+
+    /**
+       GEDF schedulability test, by Goossens, Funk, Baruah (the GFB test).
+       
+       J. Goossens, S. Funk, and S. Baruah, “Priority-driven
+       scheduling of periodic task systems on multiprocessors,” Real
+       Time Systems, vol. 25, no. 2–3, pp. 187–205, 2003.
+     */
+    template <class Iter>
+    bool gfp_bound_density(int n_proc, Iter tset_begin, Iter tset_end)
+    {
+        double beta_tot=0, lambda_max=0, lim=0;
+        //vector<FPTask> tset(tsetin);
+
+        sort_by_increasing(tset_begin, tset_end, &Task::get_dline);
+
+	for(auto k=tset_begin; k != tset_end; k++) {
+            beta_tot += lambda(*k);
+            
+            if (lambda(*k) > lambda_max){
+                lambda_max = lambda(*k);
+                lim = ((double)n_proc / 2.0)*(1 - lambda_max)+lambda_max;
+            }
+            
+            if (beta_tot > lim)
+                return false;
+	}
+	return true;
+    }
 }
 
 #endif
--- test/MEDFTest.cpp
+++ test/MEDFTest.cpp
@@ -7,12 +7,42 @@ using namespace Scan;
 
 TEST(MEDFRTA, Simple1)
 {
-    vector<Task> tset = {
-        Task(5,20,20),
-        Task(4,16,16),
-        Task(7,28,28), 
-        Task(9,36,36)
+    vector<FPTask> tset = {
+        FPTask(5,20,20),
+        FPTask(4,16,16),
+        FPTask(7,28,28), 
+        FPTask(9,36,36)
     };
 
-    EXPECT_EQ(true, rta_compl_edf(2, tset));
+    EXPECT_EQ(true, gedf_iterative(2, tset.begin(), tset.end()));
+    EXPECT_EQ(true, gedf_bound_gfb(2, tset.begin(), tset.end()));
+    EXPECT_EQ(true, gfp_bound_density(2, tset.begin(), tset.end()));
 }
+
+TEST(MEDFRTA, Simple2)
+{
+    vector<Task> tset = {
+        Task(4,6,6),
+        Task(4,6,6),
+        Task(4,6,6)
+    };
+
+    EXPECT_EQ(false, gedf_iterative(2, tset.begin(), tset.end()));
+    EXPECT_EQ(false, gedf_bound_gfb(2, tset.begin(), tset.end()));
+    EXPECT_EQ(false, gfp_bound_density(2, tset.begin(), tset.end()));
+}
+
+
+TEST(MEDFRTA, Simple2)
+{
+    vector<Task> tset = {
+        Task(4,6,6),
+        Task(4,6,6),
+        Task(4,6,6)
+    };
+
+    EXPECT_EQ(false, gedf_iterative(2, tset.begin(), tset.end()));
+    EXPECT_EQ(false, gedf_bound_gfb(2, tset.begin(), tset.end()));
+    EXPECT_EQ(false, gfp_bound_density(2, tset.begin(), tset.end()));
+}
+
